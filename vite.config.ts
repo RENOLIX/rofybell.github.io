86baf9c1fbe8cd198bb52c1769ee9ca2933a4160
asoual12 <alger12c200@gmail.com>
2026-06-17 23:53:11 +0100

    Configure rofybell.store custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/rofybell.github.io/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
